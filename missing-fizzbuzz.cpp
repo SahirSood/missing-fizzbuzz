5c2833ff85bfc45db0906da5c7fdfa5fc195786a
Zounon <46566620+Zounon@users.noreply.github.com>
2023-06-01 15:06:09 -0700

    Add files via upload

---
+++ missing-fizzbuzz.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+ 
+int main(void)
+{
+    int i;
+    for (i=1; i<=100; i++)
+    {
+         
+        // number divisible by 3? print 'Fizz'
+        // in place of the number
+        if ()
+        printf("Fizz\n");                
+         
+        // number divisible by 5, print 'Buzz' 
+        // in place of the number
+        printf("Buzz\n");                
+     
+        // number divisible by 15? print 'FizzBuzz'
+        printf ("FizzBuzz\n");   
+
+        // not divisable by 3, 5, or 15? print the number           
+        printf();                
+    }
+
+    // How should this function end?    
+}
